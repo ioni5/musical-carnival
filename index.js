c203059f4b20a05739f3d3f6531213ab95d32307
ioni5 <115875839+ioni5@users.noreply.github.com>
2023-09-24 18:39:44 +0000

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("dist"));
+
 app.get("/test", (req, res) => {
     res.status(200).send("This is a message");
 });
