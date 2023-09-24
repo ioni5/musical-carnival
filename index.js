c2f4be2e3083823ab8f18f51da2d829ae9237b92
ioni5 <115875839+ioni5@users.noreply.github.com>
2023-09-24 18:12:28 +0000

    deploy heroku

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/test", (req, res) => {
+    res.status(200).send("This is a message");
+});
+
+app.listen(9090, () => {
+    console.log("Server running on port:9090");
+})
